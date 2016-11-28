b70f4baaf68a413c76a2f93b3607b80b969d9234
幾野直人 <moon_in_france@yahoo.co.jp>
2016-11-28 21:57:24 +0900

    added date property to Date

--- src/date.js
+++ src/date.js
@@ -1,8 +1,9 @@
 export default class _Date extends Date {
     constructor(...args) {
         super(...args);
+        this.date = new Date(...args);
     }
     is(f) {
-        return f(this);
+        return f(this.date);
     }
 }
