a02be93e43279b2067ff6119c3d9b14e9d18444b
幾野直人 <moon_in_france@yahoo.co.jp>
2016-12-04 18:33:48 +0900

    added time property to Date class

--- src/date.js
+++ src/date.js
@@ -1,16 +1,20 @@
 export default class _Date {
     constructor(...args) {
         this.date = new Date(...args);
+        this.time = this.date.getTime();
     }
 
     is(f) {
         return f(this.date);
     }
+    valueOf() {
+        return this.date.valueOf();
+    };
 }
 for (const val of ['Date', 'FullYear', 'Hours', 'Milliseconds', 'Minutes', 'Month', 'Seconds', 'Time', 'UTCDate', 'UTCFullYear', 'UTCHours', 'UTCMilliseconds', 'UTCMinutes', 'UTCMonth', 'UTCSeconds']) {
     _Date.prototype['get' + val] = function () {return this.date['get' + val]();};
     _Date.prototype['set' + val] = function (...args) {
-        const newDate = new Date(this.date.getTime());
+        const newDate = new Date(this.time);
         return new _Date(newDate['set' + val].apply(newDate, args));
     };
 }
@@ -20,4 +24,3 @@ for (const val of ['Day', 'TimezoneOffset', 'UTCDay']) {
 for (const val of ['DateString', 'ISOString', 'JSON', 'LocaleDateString', 'LocaleString', 'LocaleTimeString', 'String', 'TimeString', 'UTCString']) {
     _Date.prototype['to' + val] = function() {return this.date['to' + val]();};
 }
-_Date.prototype.valueOf = function() {return this.date.valueOf();};
