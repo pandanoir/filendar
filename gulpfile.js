6c4206a202074981dd78e5ff72eac19d42fcf9fa
幾野直人 <moon_in_france@yahoo.co.jp>
2016-12-07 10:36:33 +0900

    renamed minified filename to filendar.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const closureCompiler = require('google-closure-compiler').gulp();
 gulp.task('minify', () => {
     gulp.src('./dist/filendar.js')
     .pipe(closureCompiler({
-        js_output_file: 'calendar.min.js',
+        js_output_file: 'filendar.min.js',
         compilation_level: 'SIMPLE_OPTIMIZATIONS',
         language_in: 'ECMASCRIPT6_STRICT',
         language_out: 'ECMASCRIPT5_STRICT',
